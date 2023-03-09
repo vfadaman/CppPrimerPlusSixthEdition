23f709add30e8714b5366ad521f5c954c010b6ef
vfadaman <44033480+vfadaman@users.noreply.github.com>
2023-03-10 01:03:58 +0900

    Project Start

---
+++ CppPrimerPlusSixthEdition/myfirst.cpp
@@ -0,0 +1,16 @@
+// CppPrimerPlusSixthEdition.cpp : 콘솔 응용 프로그램에 대한 진입점을 정의합니다.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	using namespace std;
+	cout << "C++의 세계로 오십시오.";
+	cout << endl;
+	cout << "후회하지 않으실 겁니다.!" << endl;
+	cin.get();
+	return 0;
+}
+
